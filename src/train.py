28afa7e599b29a0bd7df646b1c6cae7b3b55f743
yaya-sy <yayasysco@gmail.com>
2023-03-13 16:13:38 +0100

    indent

--- src/train.py
+++ src/train.py
@@ -64,9 +64,9 @@ def train(model: EntropyWhisper,
 def main():
     parser = ArgumentParser()
     parser.add_argument("-c",
-                    "--config",
-                    required=True,
-                    help="The yaml config (the same used during the training).")
+                        "--config",
+                        required=True,
+                        help="The yaml config (the same used during the training).")
     
     args = parser.parse_args()
     with open(args.config, "r") as config_file:
